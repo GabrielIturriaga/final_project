2b4ba6b5a8554b8bb4246a84d068a7db83db84f2
Liam ACres <lm655287@dal.ca>
2018-03-24 19:13:29 -0300

    Just the bare bones of the ship class

--- Java/src/Ship.java
+++ Java/src/Ship.java
@@ -1,2 +1,15 @@
 public class Ship {
+    // Attributes
+
+    //bring in things for the ship
+    public Ship(){
+        // Assign to the attribute
+    }
+    // Setters
+    public void set(){}
+    // Getters
+    // Will change the void to whatever the return value is supposed to be
+    public void get(){}
+
+
 }
