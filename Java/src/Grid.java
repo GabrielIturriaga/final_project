9f429d1863f76f977b1b6cc6135be10d38ec70c7
Liam ACres <lm655287@dal.ca>
2018-03-28 14:41:16 -0300

    Just changed the grid class and the ship point array.

--- Java/src/Grid.java
+++ Java/src/Grid.java
@@ -1,2 +1,22 @@
 public class Grid {
+    private Point [][] grid = new Point[10][10];
+    private Point point;
+    public Grid(){
+        makingGrid();
+    }
+    // This adds Point objects right now
+    public void makingGrid(){
+        for(int i=0;i<grid.length;i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                grid[i][j] = new Point(i, j);
+            }
+        }
+    }
+    public void printGrid(){
+        for(int i=0;i<grid.length;i++) {
+            for(int j=0;j<grid[i].length;j++)
+                System.out.print(grid[i][j].toString() + "\t");
+            System.out.println();
+        }
+    }
 }
--- Java/src/Ship.java
+++ Java/src/Ship.java
@@ -3,20 +3,25 @@ public abstract class Ship{
 
     private int length;
     private int HP;
-    Point point ;
+    private Point point ;
+    private Point [] points ;
     //bring in things for the ship
     public Ship(int length){
         // Assign to the attribute
         this.length = length;
         this.HP = length;
         point = new Point(1,1 );
+        points = new Point[length];
+        addPoints();
     }
     // Setters
     public void setLength(int length){this.length = length;}
 
     public void setHP(int HP){this.HP = HP;}
+
     // Getters
     // Will change the void to whatever the return value is supposed to be
+
     public int getLength(){return length;}
 
     public int getHP(){return HP;}
@@ -29,7 +34,22 @@ public abstract class Ship{
 
     public void setShipY(int y) {point.setY(y);}
 
+    public void addPoints(){
+        for(int i = 0; i < points.length; i++){
+            points[i] = point;
+        }
+    }
+    // Just Prints out the Coors for the Ship
+    public String PointsToString(){
+        String pointArray = "Point:";
+        for(int i = 0; i < points.length; i++){
+
+            pointArray += " (" + getShipX() + ", " + getShipY() + ") ";
+        }
+        return (pointArray);
+    }
+    // Just Prints out the length and the HP
     public String toString (){
-        return ("Length: " + getLength() + " HP: " + getHP() + " Point: (" + getShipX() + ", " + getShipY() + ")");
+        return ("Length: " + getLength() + " HP: " + getHP());
     }
 }
